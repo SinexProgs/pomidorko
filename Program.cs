a76b7ad13b253f10e014b0e569f89211ed4d6276
SinexProgs <sinexpts234@gmail.com>
2026-05-01 05:53:56 +0700

    Phase enum & some fields

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿namespace pomidorko;
 
+enum Phase
+{
+    Focus,
+    ShortBreak,
+    LongBreak
+}
+
 class Program
 {
+    private static Phase CurrentPhase = Phase.Focus;
+    private static int SecondsLeft = FocusDuration;
+    private static bool Running = false;
+
+    private const int FocusDuration = 25 * 60;
+    private const int ShortBreakDuration = 5 * 60;
+    private const int LongBreakDuration = 15 * 60;
+    
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
