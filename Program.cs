a17fe85d0ff787897737c1a821a8fd1f7100009d
SinexProgs <sinexpts234@gmail.com>
2026-05-01 08:46:00 +0700

    Switch Phase function implementation

--- Program.cs
+++ Program.cs
@@ -12,10 +12,12 @@ class Program
     private static Phase CurrentPhase = Phase.Focus;
     private static int SecondsLeft = FocusDuration;
     private static bool Running = false;
+    private static int CyclesLeft = CyclesTillLongBreak; 
 
     private const int FocusDuration = 25 * 60;
     private const int ShortBreakDuration = 5 * 60;
     private const int LongBreakDuration = 15 * 60;
+    private const int CyclesTillLongBreak = 4;
     
     static void Main(string[] args)
     {
@@ -34,7 +36,23 @@ class Program
 
     private static void SwitchPhase()
     {
-        
+        switch ((int)CurrentPhase)
+        {
+            case 0:
+                CurrentPhase = CyclesLeft > 1 ? (Phase)1 : (Phase)2;
+                SecondsLeft = CyclesLeft > 1 ? ShortBreakDuration : LongBreakDuration;
+                break;
+            case 1:
+                CurrentPhase = (Phase)0;
+                SecondsLeft = FocusDuration;
+                CyclesLeft--;
+                break;
+            case 2:
+                CurrentPhase = (Phase)0;
+                SecondsLeft = FocusDuration;
+                CyclesLeft = CyclesTillLongBreak;
+                break;
+        }
     }
 
     private static void PlayAlarm()
